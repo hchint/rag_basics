bb4773d155b5ac6e73f0aacf0b1ea04ab1957afa
Harish Chintada <harish.chiintada@gmail.com>
2026-07-26 18:28:57 -0500

    rag_basics_v1_5

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
 
 # LLM
 llm = ChatBedrock(
-    model_id="anthropic.claude-3-5-sonnet-20240620-v1:0",
+    model_id="amazon.nova-pro-v1:0",
     region_name=os.getenv("BEDROCK_REGION")
 )
 
