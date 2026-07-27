3266c3d939b4c43facbe17862e2248aa42c4f50d
Harish Chintada <harish.chiintada@gmail.com>
2026-07-26 19:13:53 -0500

    rag_basics_v1_8

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
 # LLM
 llm = ChatBedrock(
     model_id="amazon.nova-pro-v1:0",
-    region_name=os.getenv("BEDROCK_REGION")
+    region=os.getenv("BEDROCK_REGION")
 )
 
 st.title("🔐 Nexus Security Policy Chatbot")
